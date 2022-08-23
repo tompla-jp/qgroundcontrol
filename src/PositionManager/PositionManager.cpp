tompla-jp/qgroundcontrol

commit 7b821807acb89b519a538c2ae6188dc546a6045e

Only accept sufficiently accurate location updates (#9483)
* only accept sufficiently accurate location updates

src/PositionManager/PositionManager.cpp
@@ -78,9 +78,11 @@ void QGCPositionManager::_positionUpdated(const QGeoPositionInfo &update)
     QGeoCoordinate newGCSPosition = QGeoCoordinate();
     qreal newGCSHeading = update.attribute(QGeoPositionInfo::Direction);
 
-    if (update.isValid()) {
+    if (update.isValid() && update.hasAttribute(QGeoPositionInfo::HorizontalAccuracy)) {
         // Note that gcsPosition filters out possible crap values
-        if (qAbs(update.coordinate().latitude()) > 0.001 && qAbs(update.coordinate().longitude()) > 0.001) {
+        if (qAbs(update.coordinate().latitude()) > 0.001 &&
+            qAbs(update.coordinate().longitude()) > 0.001 &&
+            update.attribute(QGeoPositionInfo::HorizontalAccuracy) <= MinHorizonalAccuracyMeters) {
             newGCSPosition = update.coordinate();
         }
     }

src/PositionManager/PositionManager.h
@@ -21,7 +21,7 @@ class QGCPositionManager : public QGCTool {
     Q_OBJECT
 
 public:
-
+    static constexpr size_t MinHorizonalAccuracyMeters = 100;
     QGCPositionManager(QGCApplication* app, QGCToolbox* toolbox);
     ~QGCPositionManager();
 

commit 678230af5c769a62d8e520bf903b6e2e37747872

PositionManager: set UERE to enable position accuracy reporting
"QNmeaPositionInfoSource supports reporting the accuracy of the
horizontal and vertical position. To enable position accuracy reporting
an estimate of the User Equivalent Range Error associated with the NMEA
source must be set with setUserEquivalentRangeError."

Source: https://doc.qt.io/qt-6/qnmeapositioninfosource.html#details

Commit 7b821807acb89b519a538c2ae6188dc546a6045e introduced new behaviour
that broke use of external GPS. This change fixes the problem reported
in https://github.com/mavlink/qgroundcontrol/issues/10271.

src/PositionManager/PositionManager.cpp
@@ -101,6 +101,8 @@ void QGCPositionManager::setNmeaSourceDevice(QIODevice* device)
     }
     _nmeaSource = new QNmeaPositionInfoSource(QNmeaPositionInfoSource::RealTimeMode, this);
     _nmeaSource->setDevice(device);
+    // set equivalent range error to enable position accuracy reporting
+    _nmeaSource->setUserEquivalentRangeError(5.1);
     setPositionSource(QGCPositionManager::NmeaGPS);
 }
 